04a758ac70232da5a308384ac9db31f472c1510f
Hoya Wang <hoya.wang@sap.com>
2019-06-20 16:17:44 +0800

    commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  // styles: [
+  //   `
+  //   h1 {
+  //     color : #ffffff
+  //   }`,
+  //   `
+  //   .description {
+  //     font-style: italic;
+  //     color: pink
+  //   }
+  //   `
+  // ]
+  styleUrls: ['app/app.componnet.css']
 })
 export class AppComponent {}
