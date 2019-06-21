35505e52c6d0c88690f20e3241ee9bdd2d6df2c2
Hoya Wang <hoya.wang@sap.com>
2019-06-21 17:49:46 +0800

    commit

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -12,7 +12,12 @@ export class MediaItemFormComponent {
   ngOnInit() {
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(''),
+      name: new FormControl('', Validators.compose(
+        [
+          Validators.pattern('[\\w\\-\\s\\/]+'),
+          Validators.required
+        ]
+      )),
       category: new FormControl(''),
       year: new FormControl(''),
     });
